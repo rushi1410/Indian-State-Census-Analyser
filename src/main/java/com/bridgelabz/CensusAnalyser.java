edf7ae50db449f305f373aaca66b4651531ae0f9
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-08-21 19:04:24 +0530

    Added:For The Analyser To Load The Indian States Census Information From A CSV File

--- src/main/java/com/bridgelabz/CensusAnalyser.java
+++ src/main/java/com/bridgelabz/CensusAnalyser.java
@@ -1,7 +1,45 @@
 package com.bridgelabz;
 
+import java.io.IOException;
+
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+import com.bridgelabz.CensusAnalyserException.ExceptionType;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
 public class CensusAnalyser {
-public static void main(String[] args) {
-	System.out.println("Welcome To Indian State Census Analyzer");
-}
+	 public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
+	    	
+	    	try {
+	    		
+	    		if(csvFilePath.contains("txt")) {
+	    			throw new CensusAnalyserException("File must be in CSV Format", ExceptionType.CENSUS_INCORRECT_FILE_FORMAT);
+	    		}
+	    		Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+	    		CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<IndiaCensusCSV>(reader);
+	    		csvToBeanBuilder.withType(IndiaCensusCSV.class);
+	    		csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+	    		CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
+	    		Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
+	    		
+	    		int numberOfEntries = 0;
+	    		while(censusCSVIterator.hasNext()) {
+	    			numberOfEntries++;
+	    			IndiaCensusCSV censusData = censusCSVIterator.next();
+	    		}
+	    		return numberOfEntries;
+	    	}
+	    	catch(IOException e) {
+	    		throw new CensusAnalyserException(e.getMessage(), ExceptionType.CENSUS_FILE_PROBLEM);
+	    	}
+	    	catch(RuntimeException e) {
+	    		throw new CensusAnalyserException("CSV File Must Have Comma As Delimiter Or Has Incorrect Header", ExceptionType.CENSUS_WRONG_DELIMITER_OR_WRONG_HEADER);
+	    	}
+	    }
+	    
+	    
 }
